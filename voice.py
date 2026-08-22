3015a67fbaf3e03234b0aa5dfcd88876773c38df
codemaster010pro <dhruvanshjariwala@gmail.com>
2026-08-23 02:05:54 +0530

    refactor: update Assistant class and improve AI instructions for AlterEgo

--- voice.py
+++ voice.py
@@ -1,42 +1,56 @@
 from dotenv import load_dotenv
 from livekit import agents
 from livekit.agents import AgentServer, AgentSession, Agent, inference, room_io, TurnHandlingOptions
-from livekit.plugins import ai_coustics,groq,silero
+from livekit.plugins import ai_coustics,openai,deepgram
+from openai import AsyncOpenAI
+import os
 
 load_dotenv()
-
 
 class Assistant(Agent):
     def __init__(self) -> None:
         super().__init__(
-        instructions = """
-            You are AlterEgo, a ultra-fast, intelligent AI assistant and smart glasses persona.
-
-            Core Behavioral Rules:
-            1. Speak naturally, concisely, and directly—like a sharp, helpful human partner.
-            2. Keep replies brief (1 to 3 sentences maximum) unless specifically asked for a detailed breakdown.
-            3. Avoid markdown formatting, bullet points, asterisks, bold text, or code blocks in your output, as your responses will be read aloud by Text-to-Speech (TTS).
-            4. Do not use robotic filler phrases like "How can I help you today?" or "As an AI...".
-            5. Speak in clear, plain spoken English. Use natural conversational pauses where appropriate.
-        """)
+        instructions = """You are AlterEgo smart glasses AI. Answer in 1 short sentence. ASCII text only.""")
 
 server = AgentServer()
 
 @server.rtc_session(agent_name="AlterEgo")
 async def AtlerEgo(ctx: agents.JobContext):
+    groq_client = AsyncOpenAI(
+        api_key=os.getenv("GROQ_API_KEY"),
+        base_url="https://api.groq.com/openai/v1",
+    )
+    
     assistant = AgentSession(
-        stt=inference.STT(model="deepgram/nova-3"),
-        llm=groq.LLM(model="openai/gpt-oss-20b"),
+        stt=deepgram.STT(
+            model="nova-3",
+            language="en",
+            interim_results=True,
+            smart_format=False,  
+            endpointing_ms=25),
+               
+        llm=openai.LLM(
+            model="openai/gpt-oss-20b",
+            client=groq_client,
+            temperature=0.0,
+        ),
+        
         tts=inference.TTS(
-            model="inworld/inworld-tts-2",
-            voice="Ashley",
-        ),
-        vad=silero.VAD.load(
-            min_silence_duration=0.3,
-            min_speech_duration=0.05,
-        ),
+        model="cartesia",
+        voice="79a125e8-cd45-4c13-8a67-188112f4dd22",
+    ),
+        
         turn_handling=TurnHandlingOptions(
-            min_endpointing_delay=0.2,
+            turn_detection="vad",
+            endpointing={
+                "mode": "fixed",
+                "min_delay": 0.05,
+                "max_delay": 0.15,
+            },
+            preemptive_generation={
+                "enabled": True,
+                "preemptive_tts": False,
+            },
         ),
     )
 
